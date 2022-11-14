c51d280ff90fabf8b7162f1d5635fd119124e890
Falivith <falivith@gmail.com>
2022-11-14 13:04:18 -0300

    Base pra Cópia das Telas FIGMA

--- project_1/01-fundamentos-reactjs/src/App.tsx
+++ project_1/01-fundamentos-reactjs/src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import { Header } from './components/Header';
+import { Post } from './Post';
+import './global.css';
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <h1>Hello World</h1>
+    <div>
+      <Header/>
+      <Post 
+        author="Gabriel Ramires" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus saepe dicta officiis odio nemo perferendis omnis? Quod voluptas veritatis sit laboriosam sunt vero vitae at, similique quaerat molestiae, odio placeat." 
+      />
+        <Post 
+        author="Júlia Braga" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus saepe dicta officiis odio nemo perferendis omnis? Quod voluptas veritatis sit laboriosam sunt vero vitae at, similique quaerat molestiae, odio placeat." 
+      />
+    </div>
   )
 }
-
-export default App
