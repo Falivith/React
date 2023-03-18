82345494089fbe7f0f6cf7a93c2f2daffbbf8674
Gabriel Ramires <falivith@gmail.com>
2023-03-17 23:16:12 -0300

    Project Structure

--- project_3/src/App.jsx
+++ project_3/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
 
 function App() {
   return (
-    <div>
+    <div className = "App">
+      <Navbar />
       <div className = "container">
-        <Outlet/>
+        <Outlet />
       </div>
     </div>
   )
--- project_3/src/components/Navbar.jsx
+++ project_3/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import './Navbar.css'
+
 const Navbar = () => {
   return (
-    <nav>
+    <nav className = "navbar">
         <h2>
             <Link to = {'/'}>Blog</Link>
         </h2>
